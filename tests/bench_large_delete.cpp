e0c8ce9230848b343ea9a3ae691ced1d9c94a3c1
dengyi.0818 <dengyi.0818@dcarlife.com>
2025-12-02 17:45:39 +0800

    feat: tet

--- tests/bench_large_delete.cpp
+++ tests/bench_large_delete.cpp
@@ -5,12 +5,12 @@
 #include <iostream>
 
 int main() {
-  const int N = 1000000;
+  const int N = 10000000;
   TextBuffer b;
   b.lines.reserve(N);
   for (int i = 0; i < N; ++i) b.lines.emplace_back("line");
   auto t0 = std::chrono::steady_clock::now();
-  b.erase_lines(N/2 - 50000, N/2 + 50000);
+  b.erase_lines(N/2 - 500000, N/2 + 500000);
   auto t1 = std::chrono::steady_clock::now();
   std::chrono::duration<double> dt = t1 - t0;
   std::cout << "erase range took " << dt.count() << "s, lines=" << b.line_count() << "\n";
